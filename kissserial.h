c0e135aa0765b591ad014fc9e974c95be8f9f05f
wb2osz <wb2osz@comcast.net>
2017-09-11 22:38:22 -0400

    New "kissutil" application.

--- kissserial.h
+++ kissserial.h
@@ -13,7 +13,7 @@
 
 void kissserial_init (struct misc_config_s *misc_config);
 
-void kissserial_send_rec_packet (int chan, unsigned char *fbuf,  int flen, int client);
+void kissserial_send_rec_packet (int chan, int kiss_cmd, unsigned char *fbuf,  int flen, int client);
 
 void kissserial_set_debug (int n);
 
