c5526524b618242e7d2a8bcad110a6c85b05b13f
wb2osz <wb2osz@comcast.net>
2018-11-24 16:57:05 -0500

    Begin version 1.6 development.

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 
-/* Dire Wolf version 1.5 */
+/* Dire Wolf version 1.6 */
 
 #define APP_TOCALL "APDW"
 
 #define MAJOR_VERSION 1
-#define MINOR_VERSION 5
+#define MINOR_VERSION 6
 //#define EXTRA_VERSION "Beta Test"
