8e38a2328eee7b73ad0af8b2af2a62e2e09f64bc
Juan Chong <juan@highcurrent.io>
2019-05-31 00:10:56 -0400

    Added additional tests, path validation, etc. Improved documentation in script comments.

--- dw-start-cli.sh
+++ dw-start-cli.sh
@@ -5,9 +5,23 @@
 # Run this from crontab periodically to check whether direwolf is running
 # and force it to start up automatically.
 
+# In order to execute this script periodically using crontab, do the following:
+#	1.- Configure the script to your liking and save changes.
+#	2.- Type "crontab -e" without quotes in the terminal
+#	  2a.- If this is your first time editing crontab, select your preferred text editor. My favorite is nano.
+#	3.- Add the line below to the crontab file. This will force the script to run every minute.
+#
+#		* * * * * /absolute/path/to/this/file/dw-start-cli.sh >/dev/null 2>&1
+#
+#	4.- Save and close the crontab file. 
+#	5.- Optional: Use top to check whether direwolf was started. I highly recommend
+#		testing this script by manually executing it before adding it to the crontab.
+#		Execute the script manually by typing: ./dw-start-cli.sh
+
 # Versioning (this file, not direwolf version)
 #-----------
-# v1.4 - KE5WSG - removed support for the GUI and vastly simplified the script
+# v1.4 - KE5WSG - Removed support for the GUI and vastly simplified the script.
+#					Improved error checking and logging. 
 # v1.3 - KI6ZHD - added variable support for direwolf binary location
 # v1.2 - KI6ZHD - support different versions of VNC
 # v1.1 - KI6ZHD - expanded version to support running on text-only displays with 
@@ -15,6 +29,7 @@
 # v1.0 - WB2OSZ - original version for Xwindow displays only
 
 # Enable (true) / disable (false) logging everything to the log file.
+# Setting this to 'false' is recommended to keep from unnecessarily filling the log file.
 DEBUG=false
 
 #Where will be stored. The directory must be writable by non-root users.
@@ -33,6 +48,10 @@ DWPATH="/home/pi/direwolf"
 
 DWCONFIG="/home/pi/direwolf"
 
+# Name of the direwolf configuration file.
+
+DWCONFIGFILE="direwolf.conf"
+
 # Direwolf start up command. Examples for both a simple and SDR config are provided.
 #
 # 1. For normal operation as TNC, digipeater, IGate, etc.
@@ -40,7 +59,7 @@ DWCONFIG="/home/pi/direwolf"
 #    Change this command to however you wish to start Direwolf.
 #	 Be sure to use variables as necessary when building your command.
 
-DWCMD="$DWPATH/direwolf -a 100 -c $DWCONFIG/direwolf.conf"
+DWCMD="$DWPATH/direwolf -a 100 -c $DWCONFIG/$DWCONFIGFILE"
 
 #---------------------------------------------------------------
 #
@@ -55,6 +74,33 @@ DWCMD="$DWPATH/direwolf -a 100 -c $DWCONFIG/direwolf.conf"
 export PATH=/usr/local/bin:$PATH
 
 # Error checking before attempting to start direwolf
+
+# Check to see whether the direwolf directory exists
+if ! [ -d "$DWPATH" ]; then
+	echo -e "ERROR: The direwolf path (DWPATH) is invalid!  Aborting."
+	echo "-----------------------" >> $LOGFILE
+	date >> $LOGFILE
+	echo "ERROR: The direwolf path (DWPATH) is invalid!  Aborting." >> $LOGFILE
+	exit 1
+fi
+
+# Check to see whether the direwolf application exists
+if ! [ -f "$DWPATH/direwolf" ]; then
+	echo -e "ERROR: The direwolf application (DWPATH/direwolf) cannot be found!  Aborting."
+	echo "-----------------------" >> $LOGFILE
+	date >> $LOGFILE
+	echo "ERROR: The direwolf application (DWPATH/direwolf) cannot be found!  Aborting." >> $LOGFILE
+	exit 1
+fi
+
+# Check to see whether the direwolf configuration file exists
+if ! [ -f "$DWCONFIG/$DWCONFIGFILE" ]; then
+	echo -e "ERROR: The direwolf configuration file specified cannot be found!  Aborting."
+	echo "-----------------------" >> $LOGFILE
+	date >> $LOGFILE
+	echo "ERROR: The direwolf configuration file specified cannot be found!  Aborting." >> $LOGFILE
+	exit 1
+fi
 
 # Check to see whether screen is installed
 if ! type "screen" > /dev/null; then
@@ -76,7 +122,7 @@ if screen -list | grep -q "direwolf"; then
 	exit
 fi
 
-# It looks like we have everything we need to start direwolf, let's try!
+# It looks like none of the errors forced the application to exit. Let's try running direwolf!
 
 # Wait a little while in case we just rebooted and you're using an old RPi.
 # Feel free to adjust this delay as needed.
