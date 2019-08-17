bc582cf24f7a7aa04ae65dd1377f739f0e986706
Davide Gerhard <rainbow@irh.it>
2019-08-17 12:56:34 +0200

    version: use version from cmake
    in this way there is not mismatch

--- src/version.h
+++ src/version.h
@@ -3,6 +3,6 @@
 
 #define APP_TOCALL "APDW"		// Assigned by WB4APR in tocalls.txt
 
-#define MAJOR_VERSION 1
-#define MINOR_VERSION 6
+//#define MAJOR_VERSION 1
+//#define MINOR_VERSION 6
 //#define EXTRA_VERSION "Beta Test"
