e81b5ea0675c73f8a8c1e77314a863a5ae0f4489
classabbyamp <dev@placeviolette.net>
2022-05-23 21:04:04 -0400

    fix compilation on musl
    this should allow for compilation on musl libc
    
    possibly fixes #150

--- src/decode_aprs.c
+++ src/decode_aprs.c
@@ -4113,11 +4113,7 @@ static void decode_tocall (decode_aprs_t *A, char *dest)
  * models before getting to the more generic APY.
  */
 
-#if defined(__WIN32__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__APPLE__)
 	    qsort (tocalls, num_tocalls, sizeof(struct tocalls_s), tocall_cmp);
-#else
-	    qsort (tocalls, num_tocalls, sizeof(struct tocalls_s), (__compar_fn_t)tocall_cmp);
-#endif
 	  }
 	  else {
 	    if ( ! A->g_quiet) {
