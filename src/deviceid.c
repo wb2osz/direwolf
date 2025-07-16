wb2osz/direwolf

commit b26c5a4d7d4b2fd19160a1ff20cece13b2c18b13

Improve error message.

src/deviceid.c
@@ -528,7 +528,7 @@ static int mice_cmp (const void *px, const void *py)
 
 void deviceid_decode_dest (char *dest, char *device, size_t device_size)
 {
-	*device = '\0';
+	strlcpy (device, "UNKNOWN vendor/model", device_size);
 
 	if (ptocalls == NULL) {
 	  text_color_set(DW_COLOR_ERROR);
@@ -554,6 +554,7 @@ void deviceid_decode_dest (char *dest, char *device, size_t device_size)
 	  }
 	}
 
+// Not found in table.
 	strlcpy (device, "UNKNOWN vendor/model", device_size);
 
 } // end deviceid_decode_dest
@@ -610,7 +611,7 @@ static inline int strncmp_z (char *a, char *b, size_t len)
 
 void deviceid_decode_mice (char *comment, char *trimmed, size_t trimmed_size, char *device, size_t device_size)
 {
-	*device = '\0';
+	strlcpy (device, "UNKNOWN vendor/model", device_size);
 
 	if (ptocalls == NULL) {
 	  text_color_set(DW_COLOR_ERROR);

commit 81abac632493c9c52bf6f8f942bf32418a437f62

Fix deviceid test when model present but no vendor
Fixes small bug introduced in b26c5a4d7d4b2fd19160a1ff20cece13b2c18b13

Without this, the error message was appended to, and e.g. `device` could
end up as "UNKNOWN vendor/modelAFilterX" instead of the expected
"AFilterX"

src/deviceid.c
@@ -551,6 +551,11 @@ void deviceid_decode_dest (char *dest, char *device, size_t device_size)
 	      strlcat (device, " ", device_size);
 	    }
 
+	    if (ptocalls[n].vendor == NULL && ptocalls[n].model != NULL) {
+	      // If we don't have a vendor, but do have a model, wipe the error message
+	      strlcpy (device, "", device_size);
+	    }
+
 	    if (ptocalls[n].model != NULL) {
 	      strlcat (device, ptocalls[n].model, device_size);
 	    }